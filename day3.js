e3252d774793cdeec04fbf4441764737ecaa2ff2
roydejager <roydejager@live.nl>
2016-09-12 21:16:17 +0200

    improved day3 stringToArray and arrayToString

--- day3.js
+++ day3.js
@@ -1,29 +1,20 @@
 const assert = require('assert');
 
 function stringToArray(str) {
-    var array = [];
 
-    for (var i = 0; i < str.length; i++) {
-        array.push(str[i]);
-    }
-    return array
+    return str.split('');
+
 }
 
 function arrayToString(arr) {
-    var arrayToString = arr.join();
 
-    for (var i = 0; i < arrayToString.length; i++) {
-        if (arrayToString[i] == ',') {
-            arrayToString = arrayToString.replace(',', '');
-        }
-    }
-    return arrayToString
+    return arr.join('');
+
 }
 
 function removeVowelsFromStr(str) {
 
     return str.replace(/[aeiou]/g, '');
-
 
 }
 
@@ -38,11 +29,11 @@ assert.deepEqual(stringToArray('hallo'), ['h', 'a', 'l', 'l', 'o']);
 
 assert(arrayToString(['t', 'e', 's', 't']) === 'test');
 assert(arrayToString([]) === '');
-//
+
 assert(removeVowelsFromStr('testing') === 'tstng');
 assert(removeVowelsFromStr('tstng') === 'tstng');
 assert(removeVowelsFromStr('aoeui') === '');
-//
+
 assert.deepEqual(removeVowelsFromArray(['t', 'e', 'o']), ['t']);
 assert.deepEqual(removeVowelsFromArray(['a', 'i']), []);
 assert.deepEqual(removeVowelsFromArray(['t', 't']), ['t', 't']);
