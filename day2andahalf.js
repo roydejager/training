ef160fe13dcc2d41ab1c04b6b6f5d78c32f4b228
roydejager <roydejager@live.nl>
2016-09-12 14:15:53 +0200

    improved day2andahalf

--- day2andahalf.js
+++ day2andahalf.js
@@ -1,35 +1,24 @@
 const assert = require('assert');
 
 function isBalanced(str) {
-    var array = [];
 
-
-    for (var i = 0; i < str.length; i++) {
+    var string = '';
+    for (var i =0; i < str.length; i++ ) {
         if (str[i] === '(' || str[i] === ')') {
-            array.push(str[i]);
+            string += str[i];
         }
     }
 
-    var arrayToString = array.join();
-
-    for (var i = 0; i < arrayToString.length; i++) {
-        if (arrayToString[i] === ',') {
-            arrayToString = arrayToString.replace(',', '');
-        }
-    }
-
-
-
-    for (var i = 0; i < arrayToString.length; i++) {
-        var patt = new RegExp('()');
-        if (patt in arrayToString) {
-            console.log('wekjfb')
-        }
-        if (res == true ) {
-
-
-        }
-    }
+    // for (var i = 0; i < arrayToString.length; i++) {
+    //     var patt = new RegExp('()');
+    //     if (patt in arrayToString) {
+    //         console.log('wekjfb')
+    //     }
+    //     if (res == true ) {
+    //
+    //
+    //     }
+    // }
 
 
 }
